1494accbd4e63f4073bc5dbf2901f768a44e8267
wellifabio <wellifabio@hotmail.com>
2024-07-30 09:26:47 -0300

    jwt

--- api/src/routes.js
+++ api/src/routes.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-// const Colaborador = require('./controllers/colaborador');
-// const Os = require('./controllers/os');
-// const Comentario = require('./controllers/comentario');
+const Colaborador = require('./controllers/colaborador');
+const Os = require('./controllers/os');
+const Comentario = require('./controllers/comentario');
 
 
 router.get('/', (req, res) => { return res.json("API OSs respondendo") });
