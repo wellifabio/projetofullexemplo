3972738b99e0d7b729b76e0f607eb303cdf1a8ce
wellifabio <wellifabio@hotmail.com>
2024-07-30 10:06:18 -0300

    jwt

--- api/src/controllers/colaborador.js
+++ api/src/controllers/colaborador.js
@@ -1,4 +1,43 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-module.exports = {};
+const login = async (req, res) => {
+    const { matricula, pin } = req.body;
+    const colaborador = await prisma.colaborador.findFirst({
+        where: {
+            matricula: matricula,
+            pin: pin
+        }
+    });
+    if (colaborador) {
+        const token = await jwt.sign({ matricula: colaborador.matricula }, process.env.KEY, {
+            //expira em uma hora ou 3600 segundos
+            expiresIn: 3600
+        });
+        colaborador.token = token;
+        return res.json(colaborador);
+    } else {
+        return res.status(401).json({ message: 'Matrícula ou pin inválidos' });
+    }
+};
+
+const read = async (req, res) => {
+    if (req.query.matricula) {
+        const colaborador = await prisma.colaborador.findUnique({
+            where: {
+                matricula: req.query.matricula
+            }
+        });
+        return res.json(colaborador);
+    } else {
+        const colaboradores = await prisma.colaborador.findMany();
+        return res.json(colaboradores);
+    }
+};
+
+module.exports = {
+    login,
+    read
+};
--- api/src/routes.js
+++ api/src/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+const Middleware = require('./middleware/middleware');
 const Colaborador = require('./controllers/colaborador');
 const Os = require('./controllers/os');
 const Comentario = require('./controllers/comentario');
 
+router.post('/login', Colaborador.login);
 
 router.get('/', (req, res) => { return res.json("API OSs respondendo") });
 
